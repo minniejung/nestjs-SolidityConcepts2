24e6e4938011d2e4a8c6b21b85649b60297290ef
minniejung <jungmignonne@gmail.com>
2025-05-20 14:13:00 +0900

    wip

--- contract/contracts/Bank.sol
+++ contract/contracts/Bank.sol
@@ -1,1 +1,26 @@
-contract Bank {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Vault.sol";
+
+contract Bank is Vault {
+    event Withdrawn(address indexed user, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call withdraw.");
+        _;
+    }
+
+    function withdraw(uint256 _value) public onlyOwner {
+        require(
+            _value > 0 && _value <= sentValue,
+            "Insufficient balance in Vault."
+        );
+
+        sentValue -= _value;
+
+        (bool success, ) = msg.sender.call{value: _value}("");
+        require(success, "Failed to send ether");
+
+        emit Withdrawn(msg.sender, _value);
+    }
+}
--- contract/contracts/Vault.sol
+++ contract/contracts/Vault.sol
@@ -1,1 +1,51 @@
-contract Vault {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Vault {
+    address public owner;
+    uint256 public sentValue;
+    uint256 public timestamp;
+    uint256 public gasUsed;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Must send some ether.");
+
+        sentValue = msg.value;
+        timestamp = block.timestamp;
+
+        (bool success, ) = tx.origin.call{value: msg.value}("");
+        require(success, "Failed to send ether");
+    }
+
+    function getCaller() public view returns (address) {
+        return msg.sender;
+    }
+
+    function getOrigin() public view returns (address) {
+        return tx.origin;
+    }
+
+    function getBlockDetails() public view returns (uint, uint, uint, address) {
+        return (block.number, block.prevrandao, block.gaslimit, block.coinbase);
+    }
+
+    function trackGasUsage() public {
+        uint256 initialGas = gasleft();
+
+        // 가스 소비를 유도
+        uint256 sum = 0;
+        for (uint256 i = 0; i < 10; i++) {
+            sum += i;
+        }
+
+        gasUsed = initialGas - gasleft();
+    }
+
+    function generateHash(string calldata _msg) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_msg));
+    }
+}
